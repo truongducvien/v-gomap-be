a40f2352135d76575797dc24cc29c752075d182b
truongducvien <truongducvien97@gmail.com>
2024-09-24 13:51:07 +0700

    86eqbc39b Fix: add provider column to user table, fix bug cannot access fb profile link

--- src/modules/auth/auth.strategy.facebook.ts
+++ src/modules/auth/auth.strategy.facebook.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Profile } from 'passport';
 import { Strategy } from 'passport-facebook';
 import { AuthServiceUser } from './auth.interface';
+import { getPublicFbAvatarUrl } from 'src/helpers';
 
 @Injectable()
 export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
@@ -12,7 +13,7 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
       callbackURL: `${process.env.PUBLIC_API_URL}/api/auth/facebook-redirect`,
       scope: 'email',
-      profileFields: ['emails', 'photos', 'name'],
+      profileFields: ['id', 'emails', 'photos', 'name'],
     });
   }
 
@@ -22,12 +23,13 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
     profile: Profile,
     done: (err: any, user: any, info?: any) => void
   ) {
-    const { name, emails, photos } = profile;
+    const { id, name, emails, photos, provider } = profile;
     const user: AuthServiceUser = {
       email: emails?.[0].value,
       firstName: name.givenName,
       lastName: name.familyName,
-      picture: photos?.[0].value,
+      picture: photos?.[0].value ? getPublicFbAvatarUrl(id, accessToken) : null,
+      provider,
     };
     done(null, user);
   }
